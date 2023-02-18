d8f75023b61bde7fc2ec474445be682071bfd7fe
Daniel Mudge <daniel.mudge@gmail.com>
2023-02-18 13:20:07 -0800

    made a change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - TODAY - RIGHT NOW - ENTERPRISE WIDE
         </a>
       </header>
     </div>
